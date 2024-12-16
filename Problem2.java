5fa368a5fa6db62f572a0bbc17c722db6dfe98e3
jholbein300 <105812268+jholbein300@users.noreply.github.com>
2024-12-16 11:19:03 -0500

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,7 +1,7 @@
-//Write a program that asks 5 times "how old are you?"
-    //If the age is below 18, say "You cannot to gamble"
-    //If the age is above 18, print "you are able to gamble! Don't get carried away"
-    //If they enter something that is not a number, is <0, or is >80 then print "this is not a valid age"
+//Write a program that asks 5 times "How old are you?"
+    //If the age is below 18, say "You cannot gamble."
+    //If the age is above 18, print "You are able to gamble! Don't get carried away."
+    //If they enter something that is not a number, is <0, or is >80 then print "This is not a valid age."
 
 
 public class Problem2
